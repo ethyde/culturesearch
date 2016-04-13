b782e1d716067e1a2b83f7a93c2e06f9b96e3b0b
Emmanuel Plouvier <eplouvier@shivacom.fr>
2016-04-13 17:51:44 +0200

    w.i.p. : add some sample of typing

--- app/main.component.ts
+++ app/main.component.ts
@@ -1,11 +1,13 @@
-import {Component} from 'angular2/core';
+import { Component } from 'angular2/core';
 
 @Component({
     selector: 'my-app',
     // templateUrl: 'app/hero-list.component.html',
     template: `
         <h1>Choose your Cultural Domain of Search</h1>
-        <p>You search about a {{ domain }}</p>
+        <p>You search about a {{ values }}</p>
+        <input (keyup)="onKey($event)">
+        
         <my-searchbox></my-searchbox>
         <my-list></my-list>
     `,
@@ -20,5 +22,17 @@ import {Component} from 'angular2/core';
 })
 
 export class AppComponent {
-    domain = 'Movie';
+    values = 'Movie';
+
+    /*
+    // without strong typing
+    onKey(event:any) {
+        this.values += event.target.value + ' | ';
+    }
+    */
+
+    // with strong typing
+    onKey(event: KeyboardEvent) {
+        this.values += (<HTMLInputElement>event.target).value + ' | ';
+    }
 }
